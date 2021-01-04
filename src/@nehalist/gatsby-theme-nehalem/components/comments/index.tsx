237d67abc157282f7f0f63cbf1d2c9279715da99
Davide Bellone <davide.bellone>
2021-01-04 12:40:05 +0100

    aggiunto utterances e custom components

---
+++ src/@nehalist/gatsby-theme-nehalem/components/comments/index.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from "react";
+import Utterances from '../../../../components/utterances';
+
+/**
+ * Placeholder which is attached under every post. Can be shadowed to
+ * quickly integrate comments (like commento, Disqus, ...).
+ */
+// @ts-ignore
+const Comments: FunctionComponent<{ id: string }> = ({id}) =><Utterances repo={`code4it-dev/blog-comments`} />;
+
+export default Comments;
