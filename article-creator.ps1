5e9057b3682d4400ff1c871a512270fd96ffd038
Davide Bellone <davide.bellone>
2021-07-07 15:01:27 +0200

    Updated article-creator script

--- article-creator.ps1
+++ article-creator.ps1
@@ -2,10 +2,11 @@ $tmpSlug =  Read-Host -Prompt 'Article slug?'
 $currentYear = Get-Date -Format "yyyy"
 $newFolderName = $currentYear +"/" + $tmpSlug
 
-Set-Location ".\content\posts"
-
 $rootLocation = Get-Location
 $placeholderImgLocation = "\assets\img_placeholder.png"
+
+Set-Location ".\content\posts"
+
 
 git checkout master
 git pull
@@ -21,7 +22,7 @@ Add-Content article.md "---"
 Add-Content article.md "title: `"Placeholder title`""
 Add-Content article.md "path: `'/blog/$tmpSlug`'"
 Add-Content article.md "tags: []"
-Add-Content article.md "featuredImage: `"`""
+Add-Content article.md "featuredImage: `"./cover.png`""
 Add-Content article.md "excerpt : `"a description for $tmpSlug`""
 Add-Content article.md "created: 4219-11-20"
 Add-Content article.md "updated: 4219-11-20"
