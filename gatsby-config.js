b7da9606a83c0f1f6ca185422df6926d8816c207
Davide Bellone <davide.bellone>
2023-02-17 10:48:44 +0100

    Update config (to be tested)

--- gatsby-config.js
+++ gatsby-config.js
@@ -151,6 +151,28 @@ module.exports = {
         },
       },
     },
+
+    {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleAnalytics: {
+          trackingId: 'AW-409570708', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-google-analytics', // default
+          anonymize: true, // default
+          allowAdFeatures: false // default
+        },
+        googleTagManager: {
+          trackingId: 'GTM-W6KBN79', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-google-tagmanager', // default
+          dataLayerName: 'dataLayer', // default
+        },
+        // defines the environments where the tracking should be available  - default is ["production"]
+        environments: ['production', 'development']
+      },
+    },
+  
+
+
     {
       resolve: `gatsby-plugin-feed`,
       options: {
